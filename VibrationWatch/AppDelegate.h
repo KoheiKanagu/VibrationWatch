dd81fe67633f2b12b3671223ac37554f9971e657
KoheiKanagu <kk.appledeveloper@gmail.com>
2014-07-19 13:35:53 +0900

    Initial Commit

---
+++ VibrationWatch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VibrationWatch
+//
+//  Created by Kohei on 2014/07/19.
+//  Copyright (c) 2014年 KoheiKanagu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VibrationWatch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VibrationWatch
+//
+//  Created by Kohei on 2014/07/19.
+//  Copyright (c) 2014年 KoheiKanagu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
